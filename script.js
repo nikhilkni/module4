8b7e6c25c4537c84ff747bf1bb5f271ac2e5c069
nikhilkni <67895661+nikhilkni@users.noreply.github.com>
2020-07-14 13:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function (window) {
-Good Bye Jim
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i in names) {
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
