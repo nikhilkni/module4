9502b99405f7778056fcd8d337f6494bc27bf7e3
nikhilkni <67895661+nikhilkni@users.noreply.github.com>
2020-07-14 13:34:02 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,10 +1,8 @@
-(function (window) {
+function (window) {
 var helloSpeaker = {};
-
 var speakWord = "Hello";
 helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 window.helloSpeaker = helloSpeaker;
-
-})(window);
+}(window);
