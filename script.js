ab24b042cf5bbffa5350ec880f00d656e41e1f41
nikhilkni <67895661+nikhilkni@users.noreply.github.com>
2020-07-17 12:36:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function (window) {
+
+(function () {
+
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i in names) {
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
-  } else {
-     window.helloSpeaker.speak(names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
   }
 }
-}(window);
+
+})();
