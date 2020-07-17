25438af1ce3da7a1b6c1eb0e2c29065bf4c180ad
nikhilkni <67895661+nikhilkni@users.noreply.github.com>
2020-07-17 12:29:17 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,10 +1,10 @@
-(function (window) {
 
-var byeSpeaker = {};
-var speakWord = "Good Bye";
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-window.byeSpeaker = byeSpeaker;
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
 })(window);
