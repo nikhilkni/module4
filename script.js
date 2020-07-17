c606db344575058972afcf60d3aebc783849461b
nikhilkni <67895661+nikhilkni@users.noreply.github.com>
2020-07-17 12:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-(function () {
+(function myFunction() {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -14,4 +14,4 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})();
+})myFunction();
